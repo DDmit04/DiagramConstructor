9ee63ac6986c70a140a694798d80d2f0f460f08c
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-04-12 03:25:49 +0300

    add visio activator

--- DiagramConstructor/App.cs
+++ DiagramConstructor/App.cs
@@ -14,13 +14,17 @@ namespace DiagramConstructor
 
             CodeParser codeParser = new CodeParser(code);
 
-            Console.WriteLine("Parsing code...");
+            Console.WriteLine("Staring work");
+
+            Console.WriteLine("Parse code...");
             DiagramBuilder diagramBuider = new DiagramBuilder(codeParser.ParseCode());
 
-            Console.WriteLine("Building diagram...");
+            Console.WriteLine("Build diagram...");
             diagramBuider.buildDiagram();
             Console.WriteLine("Finished!");
             Console.WriteLine("Builded diagram was saved to: " + Configuration.finalFilePath);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
