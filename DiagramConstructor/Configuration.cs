dac0ede0b267fdccd39a6e82ecba461c947ebcdb
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-04-14 01:19:25 +0300

    build realise

--- DiagramConstructor/Configuration.cs
+++ DiagramConstructor/Configuration.cs
@@ -6,7 +6,7 @@ namespace DiagramConstructor
     {
         public static String customFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public static String finalFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        public static bool testRun = true;
+        public static bool testRun = false;
 
     }
 }
