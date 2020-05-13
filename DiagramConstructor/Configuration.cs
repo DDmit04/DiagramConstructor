6fde475f27fba6e6248adf9201310b3c039b5d38
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-05-14 02:10:30 +0300

    buld REALISE 1.0.4.

--- DiagramConstructor/Configuration.cs
+++ DiagramConstructor/Configuration.cs
@@ -7,7 +7,7 @@ namespace DiagramConstructor
         public static String defaultFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public static String customFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         public static String finalFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        public static bool testRun = true;
+        public static bool testRun = false;
 
     }
 }
